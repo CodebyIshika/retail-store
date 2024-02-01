7230811b4f21b00f74e109ea7f8c5b8b1c62fea4
CodeByIshika <ishika11518@gmail.com>
2024-01-31 19:24:04 -0600

    Add product abstract class

---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopFinalProject
+{
+    public abstract class Product
+    {
+        private string productName;
+        protected double priceOfProduct;
+
+        public string ProductName { get; set; }
+        public int PriceOfProduct { get; set; }
+
+        protected Product(string productName, double price)
+        {
+            this.productName = productName;
+            this.priceOfProduct = price;
+        }
+
+        public abstract void ProcessingTransaction(int quantity);
+
+        public abstract double Discount(double actualPrice);
+    }
+}
